c7fd55282960a8f353f9cda8da9860f1064e5a9f
Brandon Brasson <rocksong622@gmail.com>
2019-03-25 16:55:59 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,44 @@
 class Backer
   attr_accessor :name,:title
+
   def initialize(name)
     @name=name
     @title=[]
   end
+
+  
+    # def initialize(name)
+    #  @name = name
+    #  @backed_projects = []
+    #end
+
+     def back_project(project)
+      @backed_projects << project
+      project.backers << self
+
+     end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
+
+
